f37e53461d1f5a9c594b1fc2d341439c89a8d6e4
cr0wg4n <mcm.crw@gmail.com>
2023-05-09 16:09:49 -0400

    fix: utils test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,28 +1,13 @@
 import {describe, expect, test} from '@jest/globals'
 import { getRssUrlsFromHtmlBody, getRssUrlsFromUrl } from './index'
-import { generateGuesses } from './utils'
 
 jest.setTimeout(1_000_000_000)
-
-describe('URLS guess generator', () => {
-  const URLs = [
-    'https://www.lostiempos.com/asdasdd2ko123po21/2312323',
-    'https://www.lavoz.com.ar/politica/la-recaudacion-de-cordoba-no-se-sostiene-y-pierde-con-la-inflacion/'
-  ]
-  URLs.forEach((url) => {
-    test(url, ()=>{
-      const urls = generateGuesses(url)
-      // console.log(urls)
-      expect(urls.length).toBeGreaterThan(0)
-    })
-  })
-}) 
 
 describe('get RSS from URLs', () => {
   const URLs = [
     'https://dev-academy.com/blog',
     'https://cr0wg4n.medium.com/',
-    'https://lavozbolivia.com/'
+    'https://www.lostiempos.com/'
   ]
   URLs.forEach((url) => {
     test(url, async () => {
---
+++ src/utils.test.ts
@@ -0,0 +1,18 @@
+import {describe, expect, test} from '@jest/globals'
+import { generateGuesses } from './utils'
+
+jest.setTimeout(1_000_000_000)
+
+describe('URLS guess generator', () => {
+  const URLs = [
+    'https://www.lostiempos.com/asdasdd2ko123po21/2312323',
+    'https://www.lavoz.com.ar/politica/la-recaudacion-de-cordoba-no-se-sostiene-y-pierde-con-la-inflacion/'
+  ]
+  URLs.forEach((url) => {
+    test(url, ()=>{
+      const urls = generateGuesses(url)
+      // console.log(urls)
+      expect(urls.length).toBeGreaterThan(0)
+    })
+  })
+}) 
